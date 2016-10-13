c4ddc75d77b1b19659ac33e63b5f99d27e8643aa
Boris Gherman <boris.gherman@gmail.com>
2016-10-13 14:18:16 +0300

    Initial commit

---
+++ js/client.js
@@ -0,0 +1,30 @@
+$(function(){
+
+
+  // var window_w = $(window).width();
+  // var height_w = $(window).height();
+  // var window_w_2 = $(window).width();
+  // var locale = $('html').attr('lang');
+
+
+  // //-----------------------------------------------------------------------------------------------------------------------
+  // // --- Search Open
+
+  // $('body').on('click', '.search-trigger', function(e){
+  //   if ($(this).hasClass('open-search')) {
+  //     $(this).removeClass('open-search');
+  //     $(this).parents('.header-menu').find('.search-block').fadeOut(300);
+  //   } else {
+  //     // console.log($(this).parent('.header-menu'));
+  //     $(this).addClass('open-search');
+  //     $(this).parents('.header-menu').find('.search-block').fadeIn(300);
+  //     $(this).parents('.header-menu').find('.search-text').focus();
+  //   }
+  //   e.preventDefault();
+  // });
+
+  // --- End Lang switcher
+
+
+
+});
